13823af67372a40697deb47d3e6b4f081e9f9e99
Zererx <136253594+zerexxbaby@users.noreply.github.com>
2024-04-12 02:48:28 +0000

    Update 0-binary_tree_node.c
    aaaa

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,24 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_node - creates a binary tree node
- * @value: value to store in new node
+ * binary_tree_node - Creates a binary treeee node
+ * @value: value to store in new node 
  * @parent: parent of the node to create
- * 
- * Return: pointer to the node jdid
+ * Return: the new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *node-jdid;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = new_node->right = NULL;
-    return (new_node);
+	node-jdid = malloc(sizeof(binary_tree_t));
+	if (node-jdid == NULL)
+	{
+		return (NULL);
+	}
+	node-jdid->n = value;
+	node-jdid->parent = parent;
+	node-jdid->left = NULL;
+	node-jdid->right = NULL;
+	return (node-jdid);
 }
