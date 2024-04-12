e52823bbea1eb5adaf76121b60684b862733f47c
Zererx <136253594+zerexxbaby@users.noreply.github.com>
2024-04-12 02:44:04 +0000

    Update 0-binary_tree_node.c
    change the node name

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,13 +10,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node-jdid;
+    binary_tree_t *new_node;
 
-	node-jdid = malloc(sizeof(binary_tree_t));
-	if (node-jdid == NULL)
-		return (NULL);
-	node-jdid->parent = parent;
-	node-jdid->n = value;
-	node-jdid->left = node-jdid->right = NULL;
-	return (node-jdid);
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->parent = parent;
+    new_node->n = value;
+    new_node->left = new_node->right = NULL;
+    return (new_node);
 }
